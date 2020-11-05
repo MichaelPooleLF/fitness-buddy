f82a1c33b0ac71267e197c5099f375e5c70d9a77
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-11-05 15:33:00 -0600

    made rec-cal styling more responsive

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
   return (
-    <nav className="app-header">
+    <nav className="bg-color-blue py-3">
       <div className="container d-flex align-items-center responsive-centering">
         <i className="fas fa-dumbbell size-icon"></i>
         <h3 className="ml-2 mb-0 white">Fitness Buddy</h3>
--- client/components/recommended-cal.jsx
+++ client/components/recommended-cal.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 function RecommendedCalories(props) {
   if (props.calories) {
     return (
-      <div className="header navbar navbar-brand justify-content-between border-top">
-        <p className="mb-0">{`Recommended Daily Calories: ${props.calories}`}</p>
-        <i className="fas fa-redo cursor-pointer" onClick={props.resetCalories}></i>
+      <div className="bg-color-blue border-top py-1">
+        <div className="container recommended-cal">
+          <p className="mb-0 white">{`Recommended Daily Calories: ${props.calories}`}</p>
+          <i className="fas fa-redo cursor-pointer" onClick={props.resetCalories}></i>
+        </div>
       </div>
     );
   } else {
