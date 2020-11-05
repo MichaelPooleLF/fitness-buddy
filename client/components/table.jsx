90b39103c37e679dcb98328ac0f769f3f36de136
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-11-05 15:55:43 -0600

    simplified table to a function component

--- client/components/table.jsx
+++ client/components/table.jsx
@@ -1,47 +1,39 @@
 import React from 'react';
 import TableRow from './table-row';
 
-class Table extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    // this.handleClick = this.handleClick.bind(this);
-  }
+function Table(props) {
 
-  // handleClick() {
-  //   this.props.setView('choose');
-  // }
-
-  render() {
-    const tableData = this.props.exercises.map(item => {
-      return (
-        <TableRow
-          key={item.customExerciseId}
-          name={item.exercise}
-          description={item.description}
-          setView={this.props.setView}
-          handleDeleteClick={this.props.handleDeleteClick}
-          handleUpdateClick={this.props.handleUpdateClick}
-          id={item.customExerciseId}
-        />
-      );
-    });
-    const numberOfExercises = tableData.length;
-    if (numberOfExercises === 0) {
-      return (
-        <h3 className="text-center mt-5">No exercises added.</h3>
-      );
-    }
+  const tableData = props.exercises.map(item => {
     return (
-      <div className="overflow-auto container table-container">
-        <table className="main-table">
-          <tbody>
-            {tableData}
-          </tbody>
-        </table>
-      </div>
+      <TableRow
+        key={item.customExerciseId}
+        name={item.exercise}
+        description={item.description}
+        setView={props.setView}
+        handleDeleteClick={props.handleDeleteClick}
+        handleUpdateClick={props.handleUpdateClick}
+        id={item.customExerciseId}
+      />
+    );
+  });
+
+  const numberOfExercises = tableData.length;
+
+  if (numberOfExercises === 0) {
+    return (
+      <h3 className="text-center mt-5">No exercises added.</h3>
     );
   }
+
+  return (
+    <div className="overflow-auto container table-container">
+      <table className="main-table">
+        <tbody>
+          {tableData}
+        </tbody>
+      </table>
+    </div>
+  );
 
 }
 
