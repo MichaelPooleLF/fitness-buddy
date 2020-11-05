68e4d6fdbd22f3a30bfbe85e3868d6de2ccbdc3a
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-11-05 14:51:08 -0600

    added cursor: pointer to reset button

--- client/components/recommended-cal.jsx
+++ client/components/recommended-cal.jsx
@@ -5,7 +5,7 @@ function RecommendedCalories(props) {
     return (
       <div className="header navbar navbar-brand justify-content-between border-top">
         <p className="mb-0">{`Recommended Daily Calories: ${props.calories}`}</p>
-        <i className="fas fa-redo" onClick={props.resetCalories}></i>
+        <i className="fas fa-redo cursor-pointer" onClick={props.resetCalories}></i>
       </div>
     );
   } else {
