71d79a37ed1ac0bc656d9fbc45978f6b82191077
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-11-05 22:22:11 -0600

    restyled calorie counter for responsiveness

--- client/components/calorie-counter.jsx
+++ client/components/calorie-counter.jsx
@@ -78,82 +78,65 @@ class CalorieCounter extends React.Component {
       );
     }
     return (
-      <div>
+      <>
         <Header />
         <div className="container text-center">
-          <h1 className="calorie-title">Your Info</h1>
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <select name="genderselect" value={genderValue} onChange={this.handleChange}>
-                <option disabled value="default">Gender</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </select>
-            </div>
-            <div className="form-group">
-              <table className="calorie-table">
-                <tbody className="calorie-t-body">
-                  <tr>
-                    <td className="calorie-t-data">
-                      <label>Age</label>
-                    </td>
-                    <td className="calorie-t-data calorie-t-data-textbox">
-                      <input
-                        name="age"
-                        type="text"
-                        placeholder="Age"
-                        value={ageValue}
-                        onChange={this.handleChange}
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="calorie-t-data">
-                      <label>Weight (lbs)</label>
-                    </td>
-                    <td className="calorie-t-data calorie-t-data-textbox">
-                      <input
-                        name="weight"
-                        type="text"
-                        placeholder="Weight (lbs)"
-                        value={weightValue}
-                        onChange={this.handleChange}
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="calorie-t-data">
-                      <label>Height (inches)</label>
-                    </td>
-                    <td className="calorie-t-data calorie-t-data-textbox">
-                      <input
-                        name="height"
-                        type="text"
-                        placeholder="Height (inches)"
-                        value={heightValue}
-                        onChange={this.handleChange}
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div className="form-group">
-              <select name="activitylevel" value={activityValue} onChange={this.handleChange}>
-                <option className="calorie-option" value="default" disabled>Activity Level</option>
-                <option className="calorie-option" value="Sedentary">Sedentary</option>
-                <option className="calorie-option" value="Lightly Active">Lightly Active (1-3 days a week)</option>
-                <option className="calorie-option" value="Moderately Active">Moderately Active (3-5 days a week)</option>
-                <option className="calorie-option" value="Very Active">Very Active (6-7 days a week)</option>
-                <option className="calorie-option" value="Extra Active">Extra Active (7 days a week)</option>
-              </select>
-            </div>
-            <div className="form-group">
-              <button className="btn btn-success calorie-submit">Submit</button>
-            </div>
-          </form>
+          <div className="row my-3">
+            <h1 className="col">Calorie Calculator</h1>
+          </div>
+          <div className="row">
+            <p className='col text-center'>{'Fill out the form and press "Submit" to calculate your daily calorie needs'}</p>
+          </div>
+          <div className="row">
+            <form className="col calorie-form" onSubmit={this.handleSubmit}>
+              <div className="form-group row ">
+                <label htmlFor="gender" className="col-2 col-form-label">Gender</label>
+                <div className="col">
+                  <select className="form-control" name="gender" value={genderValue} onChange={this.handleChange}>
+                    <option disabled value="default">Select your gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
+                </div>
+              </div>
+              <div className="form-group row ">
+                <label htmlFor="age" className='col-2 col-form-label'>Age</label>
+                <div className="col">
+                  <input type="number" className="form-control" id="age" name="age" placeholder="Please enter your age..."/>
+                </div>
+              </div>
+              <div className="form-group row ">
+                <label htmlFor="weight" className='col-2 col-form-label'>Weight</label>
+                <div className="col">
+                  <input type="number" className="form-control" id="weight" name="weight" placeholder="Please enter your wieght in lbs..." />
+                </div>
+              </div>
+              <div className="form-group row ">
+                <label htmlFor="height" className='col-2 col-form-label'>Height</label>
+                <div className="col">
+                  <input type="number" className="form-control" id="height" name="height" placeholder="Please enter your height in inches..." />
+                </div>
+              </div>
+              <div className="form-group row ">
+                <label className="col-2 col-form-label" htmlFor="activity">Activity Level</label>
+                <div className="col">
+                  <select className="form-control" name="activity" value={activityValue} onChange={this.handleChange}>
+                    <option className="calorie-option" disabled value="default">Select your level of activity</option>
+                    <option className="calorie-option" value="Sedentary">Sedentary</option>
+                    <option className="calorie-option" value="Lightly Active">Lightly Active (1-3 days a week)</option>
+                    <option className="calorie-option" value="Moderately Active">Moderately Active (3-5 days a week)</option>
+                    <option className="calorie-option" value="Very Active">Very Active (6-7 days a week)</option>
+                    <option className="calorie-option" value="Extra Active">Extra Active (7 days a week)</option>
+                  </select>
+                </div>
+              </div>
+              <div className="col text-center">
+                <button className="btn btn-success btn-lg">Submit</button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 }
