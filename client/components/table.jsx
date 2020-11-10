39c1c5653a3b43deea978b19c3526b6786bf2b22
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-11-09 21:38:36 -0600

    documented table component

--- client/components/table.jsx
+++ client/components/table.jsx
@@ -3,6 +3,7 @@ import TableRow from './table-row';
 
 function Table(props) {
 
+  // returns an array of table rows corresponding to the exercises for the shown day
   const tableData = props.exercises.map(item => {
     return (
       <TableRow
@@ -19,12 +20,14 @@ function Table(props) {
 
   const numberOfExercises = tableData.length;
 
+  // if there are no exercises for the day, returns a message alerting user
   if (numberOfExercises === 0) {
     return (
       <h3 className="text-center mt-5">No exercises added.</h3>
     );
   }
 
+  // otherwise, returns table populated with exercises for the day
   return (
     <div className="container table-container">
       <table className="main-table">
