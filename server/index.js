773559eb3518196ad1b81d68469f9dc3926c3961
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-11-11 19:14:56 -0600

    removed unused endpoints, indented and documented remaining

--- server/index.js
+++ server/index.js
@@ -13,147 +13,188 @@ app.use(sessionMiddleware);
 
 app.use(express.json());
 
+// used when deploying app to check for a successfull connection
 app.get('/api/health-check', (req, res, next) => {
   db.query('select \'successfully connected\' as "message"')
     .then(result => res.json(result.rows[0]))
     .catch(err => next(err));
 });
 
+// returns an array of objects representing default exercises
 app.get('/api/routine', (req, res, next) => {
   const sql = `
-  select "e"."name" as "exercise",
-         "e"."description",
-         "exerciseId",
-         "b"."name" as "bodyPart"
-  from "exercise" as "e"
-  join "exerciseBodyPart" using ("exerciseId")
-  join "bodyPart" as "b" using ("bodyPartId")
-  `;
+    select "e"."name" as "exercise",
+           "e"."description",
+           "exerciseId",
+           "b"."name" as "bodyPart"
+    from "exercise" as "e"
+    join "exerciseBodyPart" using ("exerciseId")
+    join "bodyPart" as "b" using ("bodyPartId")
+    `;
+
   db.query(sql)
     .then(result => res.json(result.rows))
     .catch(err => next(err));
 });
 
+// returns an array of objects representing exercises added to a given day
 app.get('/api/routine/day/:dayId', (req, res, next) => {
   const dayId = req.params.dayId;
   const sql = `
-  select "d"."dayId" as "day",
-         "c"."name" as "exercise",
-         "c"."description",
-         "c"."customExerciseId"
-  from "day" as "d"
-  join "dayExercise" using ("dayId")
-  join "customExercise" as "c" using ("customExerciseId")
-  where "dayId" = $1
-  order by "customExerciseId"
-  `;
+    select "d"."dayId" as "day",
+           "c"."name" as "exercise",
+           "c"."description",
+           "c"."customExerciseId"
+    from "day" as "d"
+    join "dayExercise" using ("dayId")
+    join "customExercise" as "c" using ("customExerciseId")
+    where "dayId" = $1
+    order by "customExerciseId"
+    `;
   const params = [dayId];
+
   db.query(sql, params)
     .then(result => res.json(result.rows))
     .catch(err => next(err));
 });
 
-app.get('/api/routine/exercise/:customExerciseId', (req, res, next) => {
-  const customExerciseId = parseInt(req.params.customExerciseId, 10);
-  if (!Number.isInteger(customExerciseId) || customExerciseId <= 0) {
-    return res.status(400).json({
-      error: 'customExerciseId must be a positive integer'
-    });
-  }
-  const sql = `
-  select *
-  from "customExercise"
-  where "customExerciseId" = $1
-  `;
-  const params = [customExerciseId];
-  db.query(sql, params)
-    .then(result => {
-      const exercise = result.rows[0];
-      if (exercise) {
-        res.json(exercise);
-      } else {
-        res.status(500).json({
-          error: `Can't find exercise with customExerciseId ${customExerciseId}`
-        });
-      }
-    })
-    .catch(err => next(err));
-});
-
+// adds an exercise to the provided day
 app.post('/api/routine', (req, res, next) => {
   const name = req.body.name;
   const desc = req.body.desc;
   const dayId = parseInt(req.body.dayId, 10);
+
+  // first three if statements catch and respond to invalid properties in the request body
   if (!Number.isInteger(dayId) || dayId <= 0) {
     return res.status(400).json({
-      error: 'dayId must be a positive integer'
+      error: 'Please provide a dayId in the request body that is a positive integer'
     });
   }
+
+  if (!name) {
+    res.status(400).json({
+      error: 'Please provide a name in the request body'
+    });
+  }
+
+  if (!desc) {
+    res.status(400).json({
+      error: 'Please provide a name in the request body'
+    });
+  }
+
   if (name && desc && dayId) {
     const sql = `
-  insert into "customExercise" ("name", "description")
-  values ($1, $2)
-  returning "customExerciseId"
-  `;
+    insert into "customExercise" ("name", "description")
+    values ($1, $2)
+    returning "customExerciseId"
+    `;
     const params = [name, desc];
-    db.query(sql, params)
+
+    db.query(sql, params) // this query adds exercise to customExercise table
       .then(result1 => {
         const customExerciseId = result1.rows[0].customExerciseId;
         const sql2 = `
-        insert into "dayExercise" ("dayId", "customExerciseId")
-        values ($1, $2)
-        returning *
-        `;
+      insert into "dayExercise" ("dayId", "customExerciseId")
+      values ($1, $2)
+      returning *
+      `;
         const params2 = [dayId, customExerciseId];
-        return db.query(sql2, params2)
+
+        return db.query(sql2, params2) // this query links the exercise to the day it was added.
           .then(result2 => {
             res.json(result2.rows[0]);
           });
       })
       .catch(err => next(err));
-  } else if (!name) {
-    res.status(400).json({ error: 'need a name' });
-  } else if (!desc) {
-    res.status(400).json({
-      error: 'need a description'
-    });
   }
 });
 
-app.delete('/api/routine', (req, res, next) => {
-  const customExerciseId = parseInt(req.body.customExerciseId, 10);
-  const dayId = parseInt(req.body.dayId, 10);
+// updates an exercise from the planner in the customExercise table
+app.put('/api/routine/:customExerciseId', (req, res, next) => {
+  const customExerciseId = parseInt(req.params.customExerciseId);
+  const name = req.body.name;
+  const desc = req.body.desc;
+
+  // first 3 if statements check for valid entries in url and request body
   if (!Number.isInteger(customExerciseId) || customExerciseId <= 0) {
     return res.status(400).json({
       error: 'customExerciseId must be a positive integer'
     });
   }
-  if (!Number.isInteger(dayId) || dayId <= 0) {
-    return res.status(400).json({
-      error: 'dayId must be a positive integer'
+
+  if (!name) {
+    res.status(400).json({
+      error: 'Please provide a name in the request body'
     });
   }
+
+  if (!desc) {
+    res.status(400).json({
+      error: 'Please provide a name in the request body'
+    });
+  }
+
+  if (name && desc && customExerciseId) {
+    const sql = `
+      update "customExercise"
+      set "name" = $1,
+          "description" = $2
+      where "customExerciseId" = $3
+      returning *
+      `;
+    const params = [name, desc, customExerciseId];
+
+    db.query(sql, params)
+      .then(result => {
+        res.json(result.rows[0]);
+      })
+      .catch(err => next(err));
+  }
+});
+
+// deletes an exercise from the planner
+app.delete('/api/routine', (req, res, next) => {
+  const customExerciseId = parseInt(req.body.customExerciseId, 10);
+  const dayId = parseInt(req.body.dayId, 10);
+
+  if (!Number.isInteger(customExerciseId) || customExerciseId <= 0) {
+    return res.status(400).json({
+      error: 'Please provide a customExerciseId in the request body that is a positive integer'
+    });
+  }
+
+  if (!Number.isInteger(dayId) || dayId <= 0) {
+    return res.status(400).json({
+      error: 'Please provide a dayId in the request body that is a positive integer'
+    });
+  }
+
   const sql = `
-  delete from "customExercise"
-  where "customExerciseId" = $1
-  returning *
-  `;
+    delete from "customExercise"
+    where "customExerciseId" = $1
+    returning *
+    `;
   const params = [customExerciseId];
-  db.query(sql, params)
+
+  db.query(sql, params) // this query deletes exercise from the customExercise table
     .then(result => {
       const exercise = result.rows[0];
+
       if (!exercise) {
         res.status(404).json({
           error: `Can't find exercise with customExerciseId ${customExerciseId}`
         });
+
       } else {
         const sql2 = `
-        delete from "dayExercise"
-        where "dayId" = $1 and "customExerciseId" = $2
-        returning *
-        `;
+          delete from "dayExercise"
+          where "dayId" = $1 and "customExerciseId" = $2
+          returning *
+          `;
         const params2 = [dayId, customExerciseId];
-        return db.query(sql2, params2)
+
+        return db.query(sql2, params2) // this query deletes exercise from dayExercise table
           .then(result2 => {
             res.status(202).json(result2.rows[0]);
           });
@@ -162,64 +203,28 @@ app.delete('/api/routine', (req, res, next) => {
     .catch(err => next(err));
 });
 
-app.get('/api/routine/calories', (req, res, next) => {
-  const sql = `
-  select *
-  from "routine"
-  `;
-  db.query(sql)
-    .then(result => res.json(result.rows[0]))
-    .catch(err => next(err));
-});
-
+// updates calories in routine table with new value provided in request body
 app.put('/api/routine/calories', (req, res, next) => {
   const calories = parseInt(req.body.calories);
+
   if (!Number.isInteger(calories) || calories <= 0) {
     return res.status(400).json({
-      error: 'calories must be a positive integer'
+      error: 'calories in request body must be a positive integer'
     });
   }
+
   const sql = `
-  update "routine"
-  set "recommendedCalories" = $1
-  returning *
-  `;
+    update "routine"
+    set "recommendedCalories" = $1
+    returning *
+    `;
   const params = [calories];
+
   db.query(sql, params)
     .then(result => {
       const routine = result.rows[0];
       res.json(routine);
     });
-});
-
-app.put('/api/routine/:customExerciseId', (req, res, next) => {
-  const customExerciseId = parseInt(req.params.customExerciseId);
-  const name = req.body.name;
-  const desc = req.body.desc;
-  if (!Number.isInteger(customExerciseId) || customExerciseId <= 0) {
-    return res.status(400).json({
-      error: 'customExerciseId must be a positive integer'
-    });
-  }
-  if (name && desc && customExerciseId) {
-    const sql = `
-  update "customExercise"
-  set "name" = $1,
-      "description" = $2
-  where "customExerciseId" = $3
-  returning *
-  `;
-    const params = [name, desc, customExerciseId];
-    db.query(sql, params)
-      .then(result => {
-        res.json(result.rows[0]);
-      })
-      .catch(err => next(err));
-  } else {
-    res.status(500).json({
-      error: 'invalid input'
-    });
-  }
 });
 
 app.use('/api', (req, res, next) => {
